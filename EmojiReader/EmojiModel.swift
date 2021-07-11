85c383d2587ecfee6f46183318819ccb1784961f
Sergey <borichev.s@gmail.com>
2021-07-11 19:31:25 +0300

    initial commit

---
+++ EmojiReader/EmojiModel.swift
@@ -0,0 +1,16 @@
+//
+//  EmojiModel.swift
+//  EmojiReader
+//
+//  Created by Sergey Borichev on 15.06.2021.
+//
+
+import Foundation
+
+
+struct Emoji {
+    var emoji: String
+    var name: String
+    var description: String
+    var isFavorite: Bool
+}
---
+++ EmojiReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiReader
+//
+//  Created by Sergey Borichev on 15.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         
+    }
+
+
+}
+
